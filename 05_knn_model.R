398ac62a7435c9672859913e8fb5897749820376
clb3122 <clb3122@outlook.com>
2026-05-03 12:54:17 -0400

    Update 05_knn_model.R

--- 05_knn_model.R
+++ 05_knn_model.R
@@ -1,1 +1,45 @@
+library(tidyverse)
+library(class)
 
+set.seed(123)
+
+fit_party_knn <- function(predictors, label = "", k_max = 20) {
+  dat <- anes_model_data |>
+    filter(party_id_3 %in% c("Democrat", "Independent", "Republican")) |>
+    mutate(party_id_3 = droplevels(factor(party_id_3))) |>
+    select(party_id_3, any_of(predictors)) |>
+    drop_na()
+  # convert all predictors to numeric with dummies
+  X <- model.matrix(party_id_3 ~ ., data = dat)[, -1]
+  Y <- dat$party_id_3
+  n <- nrow(X)
+  Z <- sample(n, n / 2)
+  X.training <- X[Z, ]
+  X.testing  <- X[-Z, ]
+  Y.training <- Y[Z]
+  Y.testing  <- Y[-Z]
+  # find optimal K
+  class.rate <- rep(0, k_max)
+  for (K in 1:k_max) {
+    knn.result <- knn(X.training, X.testing, Y.training, k = K)
+    class.rate[K] <- mean(knn.result == Y.testing)
+  }
+
+  best_k <- which.max(class.rate)
+  best_rate <- class.rate[best_k]
+
+  plot(1:k_max, class.rate, type = "b",
+       xlab = "K", ylab = "Classification rate",
+       main = paste(label, "- KNN accuracy by K"))
+  abline(v = best_k, lty = 2, col = "red")
+  cat("\n", label, "KNN \n")
+  cat("Optimal K:", best_k, "\n")
+  cat("Accuracy: ", round(best_rate, 4), "\n")
+  cat("Misclassification:", round(1 - best_rate, 4), "\n")
+  knn.best <- knn(X.training, X.testing, Y.training, k = best_k)
+  print(table(Predicted = knn.best, Actual = Y.testing))
+  invisible(list(class.rate = class.rate, best_k = best_k, best_rate = best_rate))
+}
+
+demo_knn  <- fit_party_knn(demo_vars,  label = "DEMO")
+issue_knn <- fit_party_knn(issue_vars, label = "ISSUE")
