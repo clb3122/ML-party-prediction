0fc016a9c6f7d23d89bcdbc9e4d2fcc0714afdff
Graham Whitney <grahamwhitneyemail@gmail.com>
2026-05-04 10:26:08 -0400

    Added LASSO model

--- 04_lasso_model.R
+++ 04_lasso_model.R
@@ -1,1 +1,63 @@
+library(tidyverse)
+library(glmnet)
+set.seed(123)
 
+fit_party_lasso <- function(predictors, data, label = "") {
+  dat <- data |>
+    filter(party_id_3 %in% c("Democrat", "Independent", "Republican")) |>
+    mutate(party_id_3 = droplevels(factor(party_id_3))) |>
+    select(party_id_3, any_of(predictors)) |>
+    drop_na()
+  X <- model.matrix(party_id_3 ~ ., data = dat)[, -1]
+  Y <- dat$party_id_3
+  # Cross-validated LASSO
+  cvfit <- cv.glmnet(
+    X, Y,
+    alpha = 1,
+    family = "multinomial",
+    type.measure = "class")
+  best.lambda <- cvfit$lambda.min
+  # Plot lambdas against standard deviation
+  plot(CV$lambda, CV$cvsd,
+       xlab = "lambda", ylab = "CV Standard Deviation",
+       main = paste(label, "- KNN accuracy by K"))
+  abline(v = best.lambda, lty = 2, col = "red")
+  # Predictions (on training data)
+  pred.class <- predict(cvfit, newx = X, s = best.lambda, type = "class")
+  pred.class <- as.vector(pred.class)
+  # Confusion matrix
+  conf.mat <- table(Predicted = pred.class, Actual = Y)
+  # Training error
+  error.rate <- mean(pred.class != Y)
+  accuracy   <- 1 - error.rate
+  # Cross-validated error (preferred)
+  cv.error <- cvfit$cvm[which(cvfit$lambda == best.lambda)]
+  # Final model refit at best lambda
+  final.model <- glmnet(
+    X, Y,
+    alpha = 1,
+    family = "multinomial",
+    lambda = best.lambda
+  )
+  cat("\n", label, "LASSO\n")
+  cat("Optimal lambda:", best.lambda, "\n")
+  cat("Training Accuracy:", round(accuracy, 4), "\n")
+  cat("Training Misclassification:", round(error.rate, 4), "\n")
+  cat("CV Misclassification:", round(cv.error, 4), "\n\n")
+  print(conf.mat)
+  invisible(list(
+    model = final.model,
+    cvfit = cvfit,
+    best.lambda = best.lambda,
+    confusion.matrix = conf.mat,
+    training.error = error.rate,
+    cv.error = cv.error
+  ))
+}
+
+demo_lasso  <- fit_party_lasso(demo_vars,  data = anes_model_data, label = "DEMO") # Demo LASSO with Ind voters
+issue_lasso <- fit_party_lasso(issue_vars, data = anes_model_data, label = "ISSUE") # Issue LASSO with Ind voters
+
+# without independents
+demo_lasso_2party  <- fit_party_lasso(demo_vars,  data = anes_data_2party, label = "DEMO 2-party")
+issue_lasso_2party <- fit_party_lasso(issue_vars, data = anes_data_2party, label = "ISSUE 2-party")
